ead0ac9e8da71733b2fc5a697643888e87e3e082
⚡ᴄʏʙᴇʀ ᴡᴀʀʀɪᴏʀꜱ⚡ <59213352+cyberw4rriors@users.noreply.github.com>
2024-03-03 09:33:16 +0530

    Add files via upload

---
+++ wpx/app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyCfbcF1rqWcqcdej0wz4UhlSqlMWwJogDI",
+    authDomain: "bossezra-1dc97.appspot.com",
+    databaseURL: "https://bossezra-1dc97-default-rtdb.firebaseio.com",
+    projectId: "bossezra-1dc97",
+    storageBucket: "bossezra-1dc97.appspot.com",
+    messagingSenderId: "590831773559",
+    appId: "1:590831773559:android:94f809fc01b3296e7be372"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
